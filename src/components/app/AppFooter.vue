de1bb6d420cfcc0b7ab4c25ae21cc376b4e6400f
Jakub Jančík <j4jancik@gmail.com>
2022-05-13 13:24:26 +0200

    add contact in footer

--- src/components/app/AppFooter.vue
+++ src/components/app/AppFooter.vue
@@ -1,6 +1,18 @@
 <template>
   <v-footer color="primary" padless>
     <v-card class="primary flex" flat tile>
+      <v-card-text class="text-center">
+        <v-btn
+          v-for="icon in icons"
+          :key="icon.name"
+          class="mx-4 white--text"
+          icon
+        >
+          <v-icon @click="open(icon.link)" size="24px">
+            {{ icon.name }}
+          </v-icon>
+        </v-btn>
+      </v-card-text>
       <v-card-text class="py-2 white--text text-center">
         {{ new Date().getFullYear() }} © <strong>Jakub Jančík</strong>
       </v-card-text>
@@ -11,8 +23,26 @@
 export default {
   data() {
     return {
-      icons: ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"],
+      icons: [
+        {
+          name: "mdi-twitter",
+          link: "https://twitter.com/j4jakub",
+        },
+        {
+          name: "mdi-linkedin",
+          link: "https://www.linkedin.com/in/jakub-jan%C4%8D%C3%ADk-170a58188/",
+        },
+        {
+          name: "mdi-github",
+          link: "https://github.com/jakubko97/lottery",
+        },
+      ],
     };
+  },
+  methods: {
+    open(link) {
+      window.open(link, "_blank");
+    },
   },
 };
 </script>
